4a664047835695d9befffea8a42e6c8fd9cdbd43
madhukarreddygangapuram <gangamadhu1234@gmail.com>
2018-11-30 21:06:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component,lazy,Suspense/*lazy loading*/  } from 'react';
 import './App.css';
+//import Comp from './mycomp/comp';
 
+
+
+const Comp=lazy(()=>import('./mycomp/comp'));//lazyloading
 class App extends Component {
+  onClick=()=>{
+    alert(`Thanks: ${this.firstname.value}| ${this.lastname.value} submitted!`);
+  }
+  onKeyUp=(target,e)=>{
+    if(e.keyCode===13){
+       switch(target){
+         case 'firstname':
+         this.lastname.focus();
+          break;
+          case 'lastname':
+         this.age.focus();
+          break;
+          case 'age':
+         this.submit.focus();
+         break;
+          default:
+          this.firstname.focus();
+           break;
+       }
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <span>FirstName:</span>
+       <input type="text" placeholder="FirstName"
+       ref={(input)=>{this.firstname=input}} 
+       onKeyUp={this.onKeyUp.bind(this,'firstname')}/><br/>
+       </div>
+       <div>
+       <span>LastName:</span>
+       <input type="text" placeholder="LastName" ref={(input)=>{this.lastname=input}} onKeyUp={this.onKeyUp.bind(this,'lastname')}/><br/>
+       </div>
+       <div>
+       <span>Age:</span>
+       <input type="text" placeholder="Age" ref={(input)=>{this.age=input}} onKeyUp={this.onKeyUp.bind(this,'age')}/><br/>
+       <input type="button"  value="submit" onClick={this.onClick} onKeyUp={this.onKeyUp.bind(this,'submit')} /><br/>
+       </div>
+       <div>My name is madhu.....</div>
+       <Suspense fallback={<div>Lazy Loading concept......</div>}>
+       <Comp></Comp>
+       </Suspense>
       </div>
     );
   }
